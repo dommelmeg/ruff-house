ef5ed9627aab53965cf8e343be828fd01e0adf71
Meg (Dommel) Story <dommelmeg@gmail.com>
2023-08-03 12:52:12 -0400

    adds production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.routes.default_url_options = {
-  host: "https://upbeat-booking.onrender.com"
+  host: "https://ruff-house.onrender.com"
 }
 
 Rails.application.configure do
